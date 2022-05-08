6429970329fb2b9687fe9607bccf69ed3520a45a
Vince Ruby <vsruby91@gmail.com>
2022-05-08 15:18:08 -0400

    Add csv configuration

--- src/main/java/com/vincentruby/App.java
+++ src/main/java/com/vincentruby/App.java
@@ -9,6 +9,7 @@ import com.vincentruby.contract.service.CheckerService;
 import com.vincentruby.dto.InitialAddress;
 import com.vincentruby.service.AddressValidatorCheckerService;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.*;
@@ -20,6 +21,8 @@ import java.util.Map;
 public class App {
 
     private static final String API_KEY_ENV = "ADDRESS_VERIFICATION_API_KEY";
+    private static final String CSV_KEY_ENV = "CSV_TO_CHECK";
+    private static final String DEFAULT_CSV = "addresses.csv";
 
     public static void main(String[] args) {
         // setup
@@ -28,8 +31,14 @@ public class App {
         AddressValidatorClient validatorClient = addressValidatorClient(configuration);
         CheckerService checkerService = checkerService(validatorClient);
 
+        // check for csv or fallback to default
+        String filename = dotenv.get(CSV_KEY_ENV);
+        if (StringUtils.isEmpty(filename)) {
+            filename = DEFAULT_CSV;
+        }
+
         // read in addresses from csv
-        List<Address> addresses = readAddresses();
+        List<Address> addresses = readAddresses(filename);
 
         // run address checks
         if (!addresses.isEmpty()) {
@@ -56,9 +65,9 @@ public class App {
         return new AddressValidatorCheckerService(client);
     }
 
-    private static CSVReader csvReader() {
+    private static CSVReader csvReader(String filename) {
         // NOTE: 5/8/22 assuming csv has a header
-        InputStream inputStream = App.class.getResourceAsStream("/csvs/addresses.csv");
+        InputStream inputStream = App.class.getResourceAsStream("/csvs/" + filename);
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         return new CSVReaderBuilder(reader)
                 .withSkipLines(1)
@@ -69,9 +78,9 @@ public class App {
         return Dotenv.load();
     }
 
-    private static List<Address> readAddresses() {
+    private static List<Address> readAddresses(String filename) {
         List<List<String>> raw = new ArrayList<>();
-        try (CSVReader csvReader = csvReader()) {
+        try (CSVReader csvReader = csvReader(filename)) {
             String[] values;
             while ((values = csvReader.readNext()) != null) {
                 raw.add(Arrays.asList(values));
