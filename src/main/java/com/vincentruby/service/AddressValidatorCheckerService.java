47aa33819e544b26d259db4c03c73a6a58612d98
Vince Ruby <vsruby91@gmail.com>
2022-05-08 13:54:47 -0400

    Maintain insertion order

--- src/main/java/com/vincentruby/service/AddressValidatorCheckerService.java
+++ src/main/java/com/vincentruby/service/AddressValidatorCheckerService.java
@@ -5,7 +5,7 @@ import com.vincentruby.contract.dto.Address;
 import com.vincentruby.contract.service.CheckerService;
 import lombok.RequiredArgsConstructor;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,7 +21,7 @@ public class AddressValidatorCheckerService implements CheckerService {
 
     @Override
     public Map<Address, Address> checkAll(List<Address> addresses) throws Exception {
-        Map<Address, Address> checked = new HashMap<>();
+        Map<Address, Address> checked = new LinkedHashMap<>();
         for (Address address : addresses) {
             checked.put(address, check(address));
         }
