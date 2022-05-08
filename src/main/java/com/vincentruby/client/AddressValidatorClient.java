50c8b6562afc9273d5d3465a34ed45ed9cfee00a
Vince Ruby <vsruby91@gmail.com>
2022-05-08 14:21:59 -0400

    Cleanup client

--- src/main/java/com/vincentruby/client/AddressValidatorClient.java
+++ src/main/java/com/vincentruby/client/AddressValidatorClient.java
@@ -14,7 +14,9 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,26 +29,18 @@ public class AddressValidatorClient {
     private final HttpClient httpClient = new DefaultHttpClient();
 
     public Address check(Address address) throws Exception {
-        HttpPost request = new HttpPost(API_URL);
-
-        List<NameValuePair> input = new ArrayList<>();
-        input.add(new BasicNameValuePair("StreetAddress", address.getStreetAddress()));
-        input.add(new BasicNameValuePair("City", address.getCity()));
-        input.add(new BasicNameValuePair("CountryCode", "US"));
-        input.add(new BasicNameValuePair("PostalCode", address.getPostalCode()));
-        input.add(new BasicNameValuePair("APIKey", apiKey));
+        HttpPost request = buildRequest();
+        List<NameValuePair> input = buildInput(address);
 
         request.setEntity(new UrlEncodedFormEntity(input));
 
         HttpResponse response = httpClient.execute(request);
+        JSONObject jsonObject = responseToJson(response);
 
-        String output = EntityUtils.toString(response.getEntity(), "UTF-8");
+        return buildAddress(jsonObject);
+    }
 
-        JSONParser parser = new JSONParser();
-
-        Object obj = parser.parse(output);
-        JSONObject jsonObject = (JSONObject) obj;
-
+    private Address buildAddress(JSONObject jsonObject) {
         String status = (String) jsonObject.get("status");
         if (status.equalsIgnoreCase("INVALID")) {
             return new InvalidAddress();
@@ -57,5 +51,31 @@ public class AddressValidatorClient {
                     .postalCode((String) jsonObject.get("postalcode"))
                     .build();
         }
+    }
+
+    private List<NameValuePair> buildInput(Address address) throws Exception {
+        List<NameValuePair> input = new ArrayList<>();
+        input.add(new BasicNameValuePair("StreetAddress", address.getStreetAddress()));
+        input.add(new BasicNameValuePair("City", address.getCity()));
+        input.add(new BasicNameValuePair("CountryCode", "US"));
+        input.add(new BasicNameValuePair("PostalCode", address.getPostalCode()));
+        input.add(new BasicNameValuePair("APIKey", apiKey));
+
+        return input;
+    }
+
+    private HttpPost buildRequest() {
+        return new HttpPost(API_URL);
+    }
+
+    private JSONObject responseToJson(HttpResponse response) throws IOException, ParseException {
+        String output = EntityUtils.toString(response.getEntity(), "UTF-8");
+
+        JSONParser parser = new JSONParser();
+
+        Object obj = parser.parse(output);
+        JSONObject jsonObject = (JSONObject) obj;
+
+        return jsonObject;
     }
 }
