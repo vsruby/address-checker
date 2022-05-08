8d28da5bdb0bcf8469257133d20f62ef6f0f46bb
Vince Ruby <vsruby91@gmail.com>
2022-05-08 13:18:21 -0400

    Create configuration class

---
+++ src/main/java/com/vincentruby/config/AddressValidatorConfiguration.java
@@ -0,0 +1,11 @@
+package com.vincentruby.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class AddressValidatorConfiguration {
+
+    private final String apiKey;
+}
