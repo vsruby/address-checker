0c59e89e1f25c2410bca2c940001e01d4d39d7ef
Vince Ruby <vsruby91@gmail.com>
2022-05-08 14:10:03 -0400

    Implement equals and hashcode on valid address classes

--- src/main/java/com/vincentruby/dto/CorrectedAddress.java
+++ src/main/java/com/vincentruby/dto/CorrectedAddress.java
@@ -3,8 +3,30 @@ package com.vincentruby.dto;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
+import java.util.Objects;
+
 @Getter
 @SuperBuilder
 public class CorrectedAddress extends InitialAddress {
-    //
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        InitialAddress that = (InitialAddress) o;
+        return Objects.equals(city, that.city)
+                && Objects.equals(postalCode, that.postalCode)
+                && Objects.equals(streetAddress, that.streetAddress);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, postalCode, streetAddress);
+    }
 }
--- src/main/java/com/vincentruby/dto/InitialAddress.java
+++ src/main/java/com/vincentruby/dto/InitialAddress.java
@@ -4,18 +4,41 @@ import com.vincentruby.contract.dto.Address;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
+import java.util.Objects;
+
 @Getter
 @SuperBuilder
 public class InitialAddress implements Address {
 
-    private final String city;
-    private final String postalCode;
-    private final String streetAddress;
+    protected final String city;
+    protected final String postalCode;
+    protected final String streetAddress;
 
     private static final String TO_STRING_TEMPLATE = "%s, %s, %s";
 
     @Override
     public String toString() {
         return String.format(TO_STRING_TEMPLATE, getStreetAddress(), getCity(), getPostalCode());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        InitialAddress that = (InitialAddress) o;
+        return Objects.equals(city, that.city)
+                && Objects.equals(postalCode, that.postalCode)
+                && Objects.equals(streetAddress, that.streetAddress);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, postalCode, streetAddress);
     }
 }
